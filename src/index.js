ec8f96cfec2e9c6e6cc816a846e2be8db0500804
Lucas Vihuchi <lucasvihuchi@gmail.com>
2024-01-30 17:22:45 -0300

    Initialize project

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { doSomething } from './test';
+import { aFuncion } from './doestExist'; // Should trigger error
+
+doSomething();
---
+++ src/test.js
@@ -0,0 +1,6 @@
+export function doSomething() {
+  print('I did something');
+}
+
+// Should trigger error because it's not used or exported
+function otherFunction() {}
